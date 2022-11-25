952fd70c6dd69b59a81c45e7cdfc3c1dcd8e3524
DroidFreak32 <rushabshah32@gmail.com>
2022-11-26 03:14:59 +0530

    9163

--- git_config.py
+++ git_config.py
@@ -693,7 +693,8 @@ class Remote(object):
 
   def _Get(self, key, all_keys=False):
     key = 'remote.%s.%s' % (self.name, key)
-    print('DEBUG REMOTE' + self._config.GetString(key, all_keys=all_keys))
+    if self._config.GetString(key, all_keys=all_keys) is not None:
+      print('DEBUG REMOTE' + self._config.GetString(key, all_keys=all_keys))
     return self._config.GetString(key, all_keys=all_keys)
 
 
