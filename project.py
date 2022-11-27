ed970e3c56ad3dfbe935a184998d5dd34626c7da
DroidFreak32 <rushabshah32@gmail.com>
2022-11-27 08:45:53 +0530

    Support review_name in batch repo uploads

--- project.py
+++ project.py
@@ -1051,6 +1051,7 @@ class Project(object):
     if not dest_branch.startswith(R_HEADS):
       dest_branch = R_HEADS + dest_branch
 
+    branch.remote.projectname = self.review_name
     if not branch.remote.projectname:
       branch.remote.projectname = self.name
       branch.remote.Save()
